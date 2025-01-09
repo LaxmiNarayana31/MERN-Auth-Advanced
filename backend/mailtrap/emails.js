9d522bdfcd2d11ddef6663e750b84c254475d9b8
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2025-01-09 21:10:55 +0530

    add function to send welcome email using Mailtrap

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,4 +1,8 @@
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import {
+  PASSWORD_RESET_REQUEST_TEMPLATE,
+  PASSWORD_RESET_SUCCESS_TEMPLATE,
+  VERIFICATION_EMAIL_TEMPLATE,
+} from "./emailTemplates.js";
 import { mailtrapClient, sender } from "./mailtrap.config.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
@@ -21,5 +25,27 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     console.error(`Error sending verification`, error);
 
     throw new Error(`Error sending verification email: ${error}`);
+  }
+};
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "e65925d1-a9d1-4a40-ae7c-d92b37d593df",
+      template_variables: {
+        company_info_name: "Auth Company",
+        name: name,
+      },
+    });
+
+    console.log("Welcome email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending welcome email`, error);
+
+    throw new Error(`Error sending welcome email: ${error}`);
   }
 };
